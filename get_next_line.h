e16ed771ced1d2d9b83289fe2e52ee14fcb9d729
Armajchr <majchrowiczarthur@gmail.com>
2020-03-09 12:29:42 +0100

    back up gnl

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armajchr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/29 12:31:10 by armajchr          #+#    #+#             */
+/*   Updated: 2019/05/14 17:16:32 by armajchr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+# define BUFF_SIZE 80
+
+int	get_next_line(int const fd, char **line);
+
+#endif
